6ccb358e0daa8a31f48d5a4b5430d585f5214d37
pouria0015 <pouria0015@gmail.com>
2024-12-07 23:37:42 -0800

    add: functio  for delet and upload file

--- helpers/helper.php
+++ helpers/helper.php
@@ -44,3 +44,61 @@ function view_error($errData , $key, $msg)
 function generateToken(){
     return bin2hex(openssl_random_pseudo_bytes(32));
 }
+
+
+function checkList($list , $item){
+
+    if(is_array($list) && !empty($item)){
+
+        $resulte = in_array($item , $list);
+        if($resulte){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    return false;
+
+}
+
+
+function isEmpty($value){
+
+    $newValue = trim($value);
+    if($newValue === ''){
+        return true;
+    }
+    return false;
+
+}
+
+function fileUpload($fileAddress , $destinationAddress){
+
+    if(!isEmpty($fileAddress) && !isEmpty($destinationAddress)){
+        if(file_exists($fileAddress)){
+            if(move_uploaded_file($fileAddress, $destinationAddress)){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+    return false;
+}
+
+
+function fileDelete($fileAddress){
+
+    if(!isEmpty($fileAddress)){
+        if(file_exists($fileAddress)){
+            if(unlink($fileAddress)){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+    return false;
+}
